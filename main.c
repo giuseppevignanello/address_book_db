a0a549a1c5dc16e714930514b9dd7a6dd553a79f
giuseppevignanello <peppe.vignanello@gmail.com>
2025-11-22 23:25:54 +0100

    add basic create and read commands

--- main.c
+++ main.c
@@ -14,31 +14,22 @@
 
 typedef struct contact
 {
-    int id;
     char name[50];
     char email[50];
 } Contact;
 
-/* this function just create a new contact, For the moment
+/* just create a new contact, For the moment
  * it doesn't get any param, cause thery are hardcoded
  */
-Contact c_contact()
+int c_contact()
 {
     Contact c;
-    c.id = 1;
 
     char n[] = "Peppe";
     strncpy(c.name, n, sizeof(c.name) - 1);
 
     char em[] = "peppe.vignanello@gmail.com";
     strncpy(c.email, em, sizeof(c.email) - 1);
-
-    return c;
-}
-
-int main()
-{
-    Contact c = c_contact();
 
     // open binary file in append mode
     FILE *fp = fopen("contacts.db", "ab");
@@ -56,6 +47,50 @@ int main()
     {
         perror("Error writing contact");
         return 1;
+    }
+
+    fclose(fp);
+
+    return 0;
+}
+
+/* For the moment this function read all the contacts from the db.
+ * Next step is to add one or more params to query the exact contact or contacts that the user wants
+ */
+int r_contact()
+{
+    FILE *fp = fopen("contacts.db", "r");
+
+    Contact c;
+
+    while (fread(&c, sizeof(Contact), 1, fp) == 1)
+    {
+        printf("Name: %s\n Email: %s\n", c.name, c.email);
+    }
+
+    fclose(fp);
+
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        perror("You have to pass the operation type");
+        return 1;
+    }
+
+    if (strcmp(argv[1], "read") == 0)
+    {
+        // TO DO: pass an hypotetically argv[2] as param
+        return r_contact();
+    }
+
+    if (strcmp(argv[1], "create") == 0)
+    {
+        // For the moment the contact is hardcoded
+        return c_contact();
     }
 
     return 0;
