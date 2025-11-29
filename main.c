76e8fbdb69dcbd847cd7b5375af7d685921e55fa
giuseppevignanello <peppe.vignanello@gmail.com>
2025-11-29 16:39:30 +0100

    Add logic to get a query message to read or create contacts

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /*
  * address_book_database.c
  * Vestion: 1.0
- *  Author: Peppe
+ * Author: Peppe
  * Created: 2025-11-17
  * Description: This project is a basic file-based database. It is
  * made for educational purpouses. For the moment it doesn't store
@@ -18,18 +18,16 @@ typedef struct contact
     char email[50];
 } Contact;
 
-/* just create a new contact, For the moment
- * it doesn't get any param, cause thery are hardcoded
+/* just create a new contact
  */
-int c_contact()
+int c_contact(char *n, char *em)
 {
     Contact c;
 
-    char n[] = "Peppe";
     strncpy(c.name, n, sizeof(c.name) - 1);
-
-    char em[] = "peppe.vignanello@gmail.com";
+    c.name[sizeof(c.name) - 1] = '\0';
     strncpy(c.email, em, sizeof(c.email) - 1);
+    c.email[sizeof(c.email) - 1] = '\0';
 
     // open binary file in append mode
     FILE *fp = fopen("contacts.db", "ab");
@@ -54,18 +52,20 @@ int c_contact()
     return 0;
 }
 
-/* For the moment this function read all the contacts from the db.
- * Next step is to add one or more params to query the exact contact or contacts that the user wants
+/* Reading the email knowing exaclty the name
  */
-int r_contact()
+int r_contact(char *name)
 {
-    FILE *fp = fopen("contacts.db", "r");
+    FILE *fp = fopen("contacts.db", "rb");
 
     Contact c;
 
     while (fread(&c, sizeof(Contact), 1, fp) == 1)
     {
-        printf("Name: %s\n Email: %s\n", c.name, c.email);
+        if (strcmp(c.name, name) == 0)
+        {
+            printf("Name: %s\n Email: %s\n", c.name, c.email);
+        }
     }
 
     fclose(fp);
@@ -75,22 +75,33 @@ int r_contact()
 
 int main(int argc, char **argv)
 {
+    // TODO: add checks for all the values
     if (argc != 2)
     {
-        perror("You have to pass the operation type");
+        perror("You have to pass the query command");
         return 1;
     }
 
-    if (strcmp(argv[1], "read") == 0)
+    char *cmd;
+    char *name;
+    char *email;
+
+    // getting the query command
+    cmd = strtok(argv[1], " ");
+
+    /// strtok maintains an internal static pointer to the next scan position,
+    // so passing NULL continues tokenization from where it previously stopped
+    name = strtok(NULL, " ");
+    email = strtok(NULL, " ");
+
+    if (strcmp(cmd, "read") == 0)
     {
-        // TO DO: pass an hypotetically argv[2] as param
-        return r_contact();
+        return r_contact(name);
     }
 
-    if (strcmp(argv[1], "create") == 0)
+    if (strcmp(cmd, "create") == 0)
     {
-        // For the moment the contact is hardcoded
-        return c_contact();
+        return c_contact(name, email);
     }
 
     return 0;
